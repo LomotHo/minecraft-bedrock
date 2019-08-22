ad7bf114731b1c1a7893141e4d505e8ceef8bbcc
lomot <lomot@qq.com>
2019-08-22 22:10:08 +0800

    rm ops.json

--- script/start-dev.sh
+++ script/start-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -f "$DATA_PATH/ops.json" ]; then
-  cp $DEFAULT_CONFIG_PATH/ops.json $DATA_PATH/ops.json
-fi
+# if [ ! -f "$DATA_PATH/ops.json" ]; then
+#   cp $DEFAULT_CONFIG_PATH/ops.json $DATA_PATH/ops.json
+# fi
 
 if [ ! -f "$DATA_PATH/permissions.json" ]; then
   cp $DEFAULT_CONFIG_PATH/permissions.json $DATA_PATH/permissions.json
@@ -32,7 +32,7 @@ if [ ! -d "$DATA_PATH/extra/$CORE_VERSION" ]; then
   mv $EXTRA_PATH/resource_packs $DATA_PATH/extra/$CORE_VERSION
 fi
 
-ln -s $DATA_PATH/ops.json $SERVER_PATH/ops.json
+# ln -s $DATA_PATH/ops.json $SERVER_PATH/ops.json
 ln -s $DATA_PATH/permissions.json $SERVER_PATH/permissions.json
 ln -s $DATA_PATH/whitelist.json $SERVER_PATH/whitelist.json
 ln -s $DATA_PATH/server.properties $SERVER_PATH/server.properties
--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -f "$DATA_PATH/ops.json" ]; then
-  cp $DEFAULT_CONFIG_PATH/ops.json $DATA_PATH/ops.json
-fi
+# if [ ! -f "$DATA_PATH/ops.json" ]; then
+#   cp $DEFAULT_CONFIG_PATH/ops.json $DATA_PATH/ops.json
+# fi
 
 if [ ! -f "$DATA_PATH/permissions.json" ]; then
   cp $DEFAULT_CONFIG_PATH/permissions.json $DATA_PATH/permissions.json
@@ -20,7 +20,7 @@ if [ ! -d "$DATA_PATH/worlds" ]; then
   mkdir -p $DATA_PATH/worlds
 fi
 
-ln -s $DATA_PATH/ops.json $SERVER_PATH/ops.json
+# ln -s $DATA_PATH/ops.json $SERVER_PATH/ops.json
 ln -s $DATA_PATH/permissions.json $SERVER_PATH/permissions.json
 ln -s $DATA_PATH/whitelist.json $SERVER_PATH/whitelist.json
 ln -s $DATA_PATH/server.properties $SERVER_PATH/server.properties
